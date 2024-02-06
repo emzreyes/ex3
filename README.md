323cc1c66bb2ba0320f392066565ea1bb21aaab7
emzreyes <el203299@ucf.edu>
2024-02-05 20:42:57 -0500

    Created quote app

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ex3
 exercise 3 - creating a git repo from scratch 
 this is my read me file
+
+"I bear a charmed life." - Shakespeare
---
+++ index.js
@@ -0,0 +1,8 @@
+let quotes = [
+    "If my life is going to mean anything, I have to live it myself. — Rick Riordan",
+    "You can't write a script in your mind and then force yourself to follow it. You have to let yourself be. — Chimamanda Ngozi Adichie",
+    "Maybe who we are isn't so much about what we do, but rather what we're capable of when we least expect it. ― Jodi Picoult"
+]
+
+// get an integer between 0 and 2 so that it will be a different quote each time
+console.log(quotes[Math.floor(Math.random()*3)])

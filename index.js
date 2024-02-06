40960dd12d68d0fba02eefb73fd8bb37b8e5dc59
emzreyes <el203299@ucf.edu>
2024-02-05 20:43:51 -0500

    Created quote app

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let quotes = [
-    "If my life is going to mean anything, I have to live it myself. — Rick Riordan",
+    "If my life is going to mean anything, I have to live it myself. — Rick Riordan" ,
     "You can't write a script in your mind and then force yourself to follow it. You have to let yourself be. — Chimamanda Ngozi Adichie",
     "Maybe who we are isn't so much about what we do, but rather what we're capable of when we least expect it. ― Jodi Picoult"
 ]
